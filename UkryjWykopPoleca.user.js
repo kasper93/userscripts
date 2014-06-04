1d6283613e236b74e24dc82e9fad5bf9166e4709
kasper93 <kasper93@gmail.com>
2014-06-04 03:52:33 +0200

    UkryjWykopPoleca: Remove even more leftovers.

--- UkryjWykopPoleca.user.js
+++ UkryjWykopPoleca.user.js
@@ -7,7 +7,7 @@
 // @include	https://*.wykop.pl/*
 // @icon	http://img7.imagebanana.com/img/jl5vqqxv/ico.png
 // @updateURL	https://raw.githubusercontent.com/kasper93/userscripts/master/UkryjWykopPoleca.user.js
-// @version	2.0.1
+// @version	2.0.2
 // @run-at	document-end
 // ==/UserScript==
 
@@ -16,6 +16,7 @@ function main() {
         // Ukrywanie znalezisk poleconych oraz sponsorowanych
         $("a[href*='wykop.pl/reklama']").closest('li.link').remove();
         $(".paylink").closest('li.link').remove();
+        $("div[id*=adocean]").closest('div.rbl-block').parent('#fixedBox').remove();
         $("div[id*=adocean]").closest('div.rbl-block').remove();
 
         // Ukrywanie reklam w urwisku
