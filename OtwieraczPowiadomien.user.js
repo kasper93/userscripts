51951b7f23d5fa4913944099728d7ba5f2a2faf9
kasper93 <kasper93@gmail.com>
2014-06-05 00:21:27 +0200

    OtwieraczPowiadomien: Add open button for notification popup

--- OtwieraczPowiadomien.user.js
+++ OtwieraczPowiadomien.user.js
@@ -3,17 +3,28 @@
 // @namespace	https://github.com/kasper93/
 // @author	kasper93
 // @description	Otwiera wszystkie nieprzeczytane powiadomienia w kartach.
-// @include	http://*.wykop.pl/powiadomienia*
+// @include	http://*.wykop.pl/*
 // @downloadURL	https://raw.githubusercontent.com/kasper93/userscripts/master/OtwieraczPowiadomien.user.js
 // @updateURL	https://raw.githubusercontent.com/kasper93/userscripts/master/OtwieraczPowiadomien.user.js
-// @version	1.1.0
+// @version	1.2.0
 // @grant	none
 // @run-at	document-end
 // ==/UserScript==
 
 function main() {
     var button = '<li><a class="open" title="otwórz powiadomienia w kartach" href="javascript: void(0)">otwórz powiadomienia w kartach</a></li>';
+    var button1 = '<a title="otwiera nieprzeczytanie powiadomienia w kartach" href="javascript: void(0)" class="openFromNotificationsPopup">otwórz</a>';
     $('.grid-main .nav > ul:first').prepend(button);
+
+    $(document).ajaxComplete(function () {
+        $('.notificationsContainer .menu-list .buttons').append(button1);
+    });
+
+    $('.notification').on('click', '.openFromNotificationsPopup', function() {
+        $(this).closest('.menu-list').find('li.type-light-warning p a:last-child').each(function (i) {
+            window.open($(this).attr('href'));
+        });
+    });
 
     $(".open").click(function () {
         $('.type-light-warning > p > a:last-of-type').each(function (i) {
