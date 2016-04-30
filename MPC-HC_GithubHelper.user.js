a8d17ba0ba7332a1007df9fd2a1a7dee8fa4748c
Kacper Michajłow <kasper93@gmail.com>
2016-04-30 20:07:42 +0200

    MPC-HC Github Helper: Rewrite without using jQuery.

--- MPC-HC_GithubHelper.user.js
+++ MPC-HC_GithubHelper.user.js
@@ -6,30 +6,17 @@
 // @include	https://github.com/*/mpc-hc*
 // @downloadURL	https://raw.githubusercontent.com/kasper93/userscripts/master/MPC-HC_GithubHelper.user.js
 // @updateURL	https://raw.githubusercontent.com/kasper93/userscripts/master/MPC-HC_GithubHelper.user.js
-// @version	1.3.2
+// @version	2.0.1
 // @grant	none
 // @run-at	document-end
 // ==/UserScript==
 
-function main($) {
-    try {
-        $(function () {
-            replace();
-
-            $(document).on('pjax:complete', function () {
-                replace();
-            });
-        });
-
-        function replace() {
-            $('.commit-desc, .commit-title, .comment-content').each(function () {
-                $(this).html($(this).html().replace(/#(\d{1,4})\b(?![^<]*?<\/a>)/gi, '<a target="_blank" href="https://trac.mpc-hc.org/ticket/$1">$&</a>'));
-            });
-        }
-    } catch (e) {
-        prompt("Something went wrong, report this issue on https://github.com/kasper93/userscripts/issues", e.message);
+function replace_ticket_number_with_link() {
+    var elems = document.querySelectorAll('.commit-desc, .commit-title, .comment-content');
+    for (var i = 0; i < elems.length; ++i) {
+        elems[i].innerHTML = elems[i].innerHTML.replace(/#(\d{1,4})\b(?![^<]*?<\/a>)/gi, '<a target="_blank" href="https://trac.mpc-hc.org/ticket/$1">$&</a>');
     }
 }
 
-// jQueryLoader, see https://github.com/kasper93/userscripts for unminified version.
-function a(){this.message="unsafeWindow failed!";this.name="Exception"}try{main(jQuery)}catch(b){console.log(b.message);try{if("undefined"===typeof unsafeWindow.jQuery)throw new a;main(unsafeWindow.jQuery)}catch(c){console.log(c.message);try{var d=document.createElement("script");d.textContent="("+main.toString()+")(window.jQuery);";document.body.appendChild(d)}catch(e){console.log(e.message)}}};
+window.onload = replace_ticket_number_with_link;
+window.addEventListener('pjax:complete', replace_ticket_number_with_link, false);
