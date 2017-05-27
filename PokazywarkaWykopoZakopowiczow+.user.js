57029bf597ee9192f46eb0108f4d05b3b8ea39d3
Kacper Michajłow <kasper93@gmail.com>
2017-05-27 13:41:33 +0200

    Optimize little bit.

--- PokazywarkaWykopoZakopowiczow+.user.js
+++ PokazywarkaWykopoZakopowiczow+.user.js
@@ -1,73 +1,80 @@
 // ==UserScript==
-// @name	Pokazywarka wykopo i zakopowiczów +
-// @namespace	https://github.com/kasper93/
-// @author	kasper93, oparte na pomyśle i wykonaniu przez owczareknietrzymryjski
-// @description	Skrypt pokazuje przy komentarzach czy ktoś zakopał / wykopał dane znalezisko 
+// @name    Pokazywarka wykopo i zakopowiczów +
+// @namespace   https://github.com/kasper93/
+// @author  kasper93, oparte na pomyśle i wykonaniu przez owczareknietrzymryjski
+// @description Skrypt pokazuje przy komentarzach czy ktoś zakopał / wykopał dane znalezisko
 // @include     http://*wykop.pl/link/*
 // @include     https://*wykop.pl/link/*
-// @downloadURL	https://raw.githubusercontent.com/kasper93/userscripts/master/PokazywarkaWykopoZakopowiczow+.user.js
-// @updateURL	https://raw.githubusercontent.com/kasper93/userscripts/master/PokazywarkaWykopoZakopowiczow+.user.js
-// @version	1.2.2
-// @grant	none
-// @run-at	document-end
+// @downloadURL https://raw.githubusercontent.com/kasper93/userscripts/master/PokazywarkaWykopoZakopowiczow+.user.js
+// @updateURL   https://raw.githubusercontent.com/kasper93/userscripts/master/PokazywarkaWykopoZakopowiczow+.user.js
+// @version 1.3.0
+// @grant   none
+// @run-at  document-end
 // ==/UserScript==
 
 function main($) {
-    wykop._ajaxCall2 = function (url, action) {
+    const wykop_ = wykop || unsafeWindow.wykop;
+
+    const createMap = function(r, s) {
+        const ret = new Map();
+        let m;
+        while (m = r.exec(s)) {
+            ret.set(m[1], m[2]);
+        }
+        return ret;
+    };
+
+    const createSet = function(r, s) {
+        const ret = new Set();
+        let m;
+        while (m = r.exec(s)) {
+            ret.add(m[1]);
+        }
+        return ret;
+    };
+
+    const ajaxCall = function(url, action, $this) {
         if (!url.match(/hash\//)) {
             if (url.match(/\?/)) {
-                url = url.replace("?", '/hash/' + wykop.params.hash + "?")
+                url = url.replace("?", '/hash/' + wykop_.params.hash + "?")
             } else {
-                url += '/hash/' + wykop.params.hash;
+                url += '/hash/' + wykop_.params.hash;
             }
         }
+
         $.ajax(url, {
-            dataType: 'text'
-        }).done(function (r) {
-            if (r.indexOf('for(;;);') === 0) {
-                // Why? Because #maciejpsuje, that's why.
-                r = r.slice(8);
-            }
-            r = $.parseJSON(r);
-            var $this = $('div[data-type="comment"] .showProfileSummary b', '.comments-stream');
-            switch (action) {
-            case 'zakopy':
-                var reason = [];
-                var people = szukaj(/<b>(.*)<\/b>/g, r.operations[2].html);
-                var reasons = szukaj(/([a-zęść ]+)<br>/g, r.operations[2].html);
-                $this.filter(function () {
-                    var n = people.indexOf($(this).text());
-                    if (n != -1) {
-                        reason.push(reasons[n]);
-                        return true;
-                    }
-                }).closest('.author').each(function (i) {
-                    $(this).append('<b class="small color-2">(' + reason[i] + ')</b>');
-                });
-                break;
-            case 'wykopy':
-                var people = szukaj(/<b>(.*)<\/b>/g, r.operations[2].html);
-                $this.filter(function () {
-                    return people.indexOf($(this).text()) != -1;
-                }).closest('.author').append('<b class="small color-0">(wykopał)</b>');
-                break;
+            dataType: 'json',
+            dataFilter: (data) => (data.startsWith('for(;;);') ? data.substring(8) : data),
+            success: (r) => {
+                if (r.error)
+                    return;
+                switch (action) {
+                case 0: {
+                    const w_set = createSet(/<b>(.*)<\/b>/g, r.operations[2].html);
+                    $this.filter(function() { return w_set.has(this.textContent) })
+                         .closest('.author')
+                         .append('<b class="small color-0">(wykopał)</b>');
+                    break;
+                }
+                case 1: {
+                    const z_map = createMap(
+                        /<b>(.*)<\/b>\s*<\/span>\s*<span class="info">\s*([^<]*)<br>/g, r.operations[2].html);
+                    $this.each(function() {
+                        if (z_map.has(this.textContent))
+                            $(this).closest('.author')
+                                   .append(`<b class="small color-2">(${z_map.get(this.textContent)})</b>`);
+                    });
+                    break;
+                }
+                }
             }
         });
-    }
-
-    $(function () {
-        var id = /link\/(\d*)\//.exec(document.location.pathname)[1];
-        wykop._ajaxCall2("//www.wykop.pl/ajax2/links/Upvoters/" + id, "wykopy");
-        wykop._ajaxCall2("//www.wykop.pl/ajax2/links/Downvoters/" + id, "zakopy");
-    });
-
-    function szukaj(r, s) {
-        var a = [], m;
-        while (m = r.exec(s)) {
-            a.push(m[1]);
-        }
-        return a;
     };
+
+    const id = /link\/(\d+)\//.exec(document.location.pathname)[1];
+    const $this = $('div[data-type="comment"] .showProfileSummary b', '.comments-stream');
+    ajaxCall(`//www.wykop.pl/ajax2/links/Upvoters/${id}`, 0, $this);
+    ajaxCall(`//www.wykop.pl/ajax2/links/Downvoters/${id}`, 1, $this);
 };
 
 // jQueryLoader, see https://github.com/kasper93/userscripts for unminified version.
