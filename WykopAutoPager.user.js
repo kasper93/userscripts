09189782486e9f8fff4dd4b2b1986ac2ec34ce4a
Kacper Michajłow <kasper93@gmail.com>
2018-04-22 16:10:38 +0200

    WykopAutoPager: Rewrite

--- WykopAutoPager.user.js
+++ WykopAutoPager.user.js
@@ -9,75 +9,108 @@
 // @exclude	/^http://[a-z]+\.wykop\.pl/(ramka|link|dodaj)/*/
 // @downloadURL	https://raw.githubusercontent.com/kasper93/userscripts/master/WykopAutoPager.user.js
 // @updateURL	https://raw.githubusercontent.com/kasper93/userscripts/master/WykopAutoPager.user.js
-// @version	3.1.2
+// @version	4.0.0
 // @grant	none
-// @run-at	document-end
+// @run-at	document-idle
 // ==/UserScript==
 
-function main($) {
-    wykop = wykop || unsafeWindow.wykop;
-    var nastepnaStronaButton = $("a.button:contains('następna')");
-    if (nastepnaStronaButton.length) {
-        var enabled = true;
-        var footerHeight = 200;
-        var pager = nastepnaStronaButton.closest('div');
-        wykop.height = {
-            window: $(window).innerHeight(),
-            document: $(document).height()
+(() => {
+    let pager = document.querySelector('.pager');
+    if (!pager) return;
+    let nextPageButton = pager.querySelector('a.button:last-child');
+    let nextPageAvailable = nextPageButton.innerText === 'następna';
+    if (!nextPageAvailable) return;
+
+    const wykop_ = wykop || unsafeWindow.wykop;
+    const $_ = jQuery || unsafeWindow.jQuery;
+    let enabled = true;
+
+    const rightFooter = `
+        <div id="rightFooterBox" class=" reset-decor reset-space dnone">
+            <a class="button big" href="#">
+                <span id="pageNum" class="active-counter"></span>
+                <i class="fa fa-chevron-up"></i>
+            </a>
+            <h4 class="clear-bottom tspace">
+                Wykop.pl © 2018
+                    (<a href="http://www.wykop.pl/dodatki/pokaz/99/">Wykop Autopager</a>
+                    by <a href="http://www.wykop.pl/ludzie/kasper93/">kasper93</a>)
+            </h4>
+            <ul class="sub-menu inline-list">
+                <li><a id="autopager" title="Wyłącz paginację" href="javascript:void(0)">${enabled ? 'Wyłącz' : 'Włącz'} paginację</a></li>
+                <li><a href="https://www.wykop.pl/info/" title="idea wykopu">Idea Wykopu</a></li>
+                <li><a href="https://www.wykop.pl/regulamin/" title="regulamin wykopu">Regulamin</a></li>
+                <li><a href="https://www.wykop.pl/polityka-prywatnosci/" title="nasza polityka prywatności">Polityka prywatności</a></li>
+                <li><a href="https://www.wykop.pl/najczesciej-zadawane-pytania/" title="pomoc i faq">Pomoc i FAQ</a></li>
+                <li><a href="https://www.wykop.pl/kontakt/" title="skontaktuj się z nami">Kontakt</a></li>
+                <li><a href="https://www.wykop.pl/ranking/">Ranking użytkowników</a></li>
+                <li><a href="https://www.wykop.pl/osiagniecia/" title="wykopowe osiągnięcia">Osiągnięcia</a></li>
+                <li><a href="https://www.wykop.pl/tagi/" title="tagi">Tagi</a></li>
+                <li><a href="https://www.wykop.pl/dodatki/" title="dodatki">Dodatki</a></li>
+                <li><a href="https://www.wykop.pl/dla-programistow/" title="Wykopywarka, Widget i API">Dla programistów</a></li>
+            </ul>
+        </div>
+    `;
+
+    const footerHeight = 200;
+    const itemsStream = document.getElementById('itemsStream');
+    const notifications = document.querySelector('.menu-list.notification');
+    let addons = document.querySelector('.grid-main > div.addons');
+    addons = addons && addons.parentNode;
+
+    let pageNum = null;
+    const grid_right = document.getElementsByClassName('grid-right')[0];
+    if (grid_right) {
+        grid_right.insertAdjacentHTML('beforeend', rightFooter);
+        pageNum = document.getElementById('pageNum');
+        pageNum.innerText = pager.querySelector('a.button.selected').innerText;
+        wykop_.turnOnRightFooter();
+        document.getElementById('autopager').onclick = (e) => {
+            enabled = !enabled;
+            e.target.title = e.target.innerText = `${enabled ? 'Wyłącz' : 'Włącz'} paginację`;
         };
-        $(window).resize(function () {
-            wykop.height.window = $(window).innerHeight();
-        });
-        wykop.nextPageLoading = false;
-
-        wykop.pobierzStrone = function (url) {
-            $.ajax(url).done(function (data) {
-                wykop.nowaStrona(data);
-            });
-        }
-
-        wykop.nowaStrona = function (html) {
-            var $html = $(html);
-            var $items = $html.find('#itemsStream li.iC');
-            $('#itemsStream').append($items);
-            wykop.bindSurveyForm($items);
-            $('.grid-main').append($html.find('.grid-main div.addons'));
-            $('.menu-list.notification').append($html.find('.grid-main .menu-list.notification li'));
-            var pagerOld = pager;
-            nastepnaStronaButton = $html.find("a.button:contains('następna')");
-            pager = nastepnaStronaButton.closest('div');
-            if (!nastepnaStronaButton.length) {
-                // ostatnia strona, szykamy pagera po przycisku "poprzednia"
-                pager = $html.find("a.button:contains('poprzednia')").closest('div');
-            }
-            pagerOld.replaceWith(pager);
-            if ($.isFunction($.ukryj)) {
-                $.ukryj();
-            }
-            $("#pageNum").text(pager.find("a.button.selected").text());
-            wykop.height.document = $(document).height();
-            wykop.bindLazy();
-            wykop.nextPageLoading = false;
-        }
-
-        $(window).bind('scroll.pagination', function () {
-            if (enabled && nastepnaStronaButton.length && !wykop.nextPageLoading && $(window).scrollTop() >= wykop.height.document - wykop.height.window - footerHeight) {
-                wykop.nextPageLoading = true;
-                wykop.pobierzStrone(nastepnaStronaButton.attr('href'));
-            }
-        });
-
-        var rightFooter = '<div class="reset-decor reset-space dnone fixed right-footer" id="rightFooterBox" style="display: none;"><a href="#" class="button big"><div id="pageCounter"><span style="border-radius: 12px;height: 24px;line-height: 24px;margin: 0;padding: 0;right: -12px;text-align: center;top: -12px;vertical-align: middle;width: 24px;background: none repeat scroll 0 0 #e74c3c;bottom: 34px;color: #fff;display: inline-block;font-size: 10px;position: absolute;" id="pageNum"></span></div><i class="fa fa-chevron-up"></i></a><h4 class="clear-bottom tspace">Wykop.pl &copy; 2014 (<a href="http://www.wykop.pl/dodatki/pokaz/99/">Wykop Autopager</a> by <a href="http://www.wykop.pl/ludzie/kasper93/">kasper93</a>)</h4><ul class="sub-menu inline-list"><li><a id="autopager" title="Wyłącz paginację" href="javascript:void(0)">Wyłącz paginację</a><li><a title="idea wykopu" href="http://www.wykop.pl/info/">Idea Wykopu</a></li><li><a title="regulamin wykopu" href="http://www.wykop.pl/regulamin/">Regulamin</a></li><li><a title="nasza polityka prywatności" href="http://www.wykop.pl/polityka-prywatnosci/">Polityka prywatności</a></li><li><a title="pomoc i faq" href="http://www.wykop.pl/najczesciej-zadawane-pytania/">Pomoc i FAQ</a></li><li><a title="skontaktuj się z nami" href="http://www.wykop.pl/kontakt/">Kontakt</a></li><li><a href="http://www.wykop.pl/ranking/">Ranking użytkowników</a></li><li><a title="wykopowe osiągnięcia" href="http://www.wykop.pl/osiagniecia/">Osiągnięcia</a></li> <li><a title="tagi" href="http://www.wykop.pl/tagi/">Tagi</a></li><li><a title="dodatki" href="http://www.wykop.pl/dodatki/">Dodatki</a></li><li><a title="Wykopywarka, Widget i API" href="http://www.wykop.pl/dla-programistow/">Dla programistów</a></li></ul></div>';
-
-        $('.grid-right').append(rightFooter);
-        $("#pageNum").text(pager.find("a.button.selected").text());
-        $('#autopager').click(function () {
-            enabled = !($(this).attr("title") == "Wyłącz paginację");
-            $(this).attr("title", (enabled ? 'Wyłącz' : 'Włącz') + " paginację");
-            $(this).text((enabled ? 'Wyłącz' : 'Włącz') + " paginację");
-        });
     }
-}
 
-// jQueryLoader, see https://github.com/kasper93/userscripts for unminified version.
-function a(){this.message="unsafeWindow failed!";this.name="Exception"}try{main(jQuery)}catch(b){console.log(b.message);try{if("undefined"===typeof unsafeWindow.jQuery)throw new a;var wykop=unsafeWindow.wykop;main(unsafeWindow.jQuery)}catch(c){console.log(c.message);try{var d=document.createElement("script");d.textContent="("+main.toString()+")(window.jQuery);";document.body.appendChild(d)}catch(e){console.log(e.message)}}};
+    const newPage = (doc) => {
+        const appendChildren = (parent, children) =>
+            children.forEach((c) => parent.appendChild(c));
+        const append = (parent, selector) =>
+            parent && appendChildren(parent, doc.querySelectorAll(selector));
+
+        let itemsStreamElems = null;
+        if (itemsStream) {
+            itemsStreamElems = doc.querySelectorAll('#itemsStream > li.iC');
+            appendChildren(itemsStream, itemsStreamElems);
+        }
+        append(notifications, '.menu-list.notification > li');
+        append(addons, '.grid-main > div.addons');
+
+        const pagerOld = pager;
+        pager = doc.querySelector('.pager');
+        nextPageButton = pager.querySelector('a.button:last-child');
+        nextPageAvailable = nextPageButton.innerText === 'następna';
+        pageNum && (pageNum.innerText = pager.querySelector('a.button.selected').innerText);
+        if (addons) {
+            pagerOld.remove();
+            addons.appendChild(pager);
+        } else {
+            pagerOld.replaceWith(pager);
+        }
+        if ($_ && typeof $_.ukryj == 'function')
+            $_.ukryj();
+        wykop_.height.document = document.documentElement.getBoundingClientRect().height;
+        itemsStreamElems && $_ && wykop_.bindSurveyForm($_('div.survey', itemsStreamElems));
+        wykop_.bindLazy();
+        wykop_.nextPageLoading = false;
+    };
+
+    window.onscroll = () => {
+        if (enabled && nextPageAvailable && !wykop_.nextPageLoading
+            && document.documentElement.scrollTop >= wykop_.height.document - wykop_.height.window - footerHeight) {
+            wykop_.nextPageLoading = true;
+            fetch(nextPageButton.href, { credentials: 'same-origin' })
+            .then((resp) => resp.text())
+            .then((resp) => newPage((new DOMParser()).parseFromString(resp, 'text/html')));
+        }
+    };
+})();
