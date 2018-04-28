c939f114278cfd4abd84c210a6f058378534d759
Kacper Michajłow <kasper93@gmail.com>
2018-04-29 01:16:36 +0200

    WykopPokazObrazki: Little update.

--- WykopPokazObrazki.user.js
+++ WykopPokazObrazki.user.js
@@ -3,25 +3,21 @@
 // @namespace	https://github.com/kasper93/
 // @author	kasper93
 // @description	Umożliwia załadowanie wszystkich obrazków/filmików w komentarzach/wpisach za pomoca tylko jednego przycisku.
-// @include	http://*.wykop.pl/link*
-// @include	http://*.wykop.pl/mikroblog*
-// @include	http://*.wykop.pl/wpis*
-// @include	http://*.wykop.pl/tag*
-// @include	https://*.wykop.pl/link*
-// @include	https://*.wykop.pl/mikroblog*
-// @include	https://*.wykop.pl/wpis*
-// @include	https://*.wykop.pl/tag*
+// @include	http://*.wykop.pl/*
+// @include	https://*.wykop.pl/*
 // @downloadURL	https://raw.githubusercontent.com/kasper93/userscripts/master/WykopPokazObrazki.user.js
 // @updateURL	https://raw.githubusercontent.com/kasper93/userscripts/master/WykopPokazObrazki.user.js
-// @version	2.0.1
+// @version	2.0.3
 // @grant	none
 // @run-at	document-idle
 // ==/UserScript==
 
+'use strict';
+
 (() => {
     const bLoadImg = true;
     const bLoadVid = false;
-    const bRemoveWatermark = true;
+    const bUseExternalSourceUrl = false;
 
     const settings = 'autoLoadImages';
     let auto = localStorage.getItem(settings) === 'true' || false;
@@ -30,39 +26,48 @@
         if (!mc.classList.contains('media-content'))
             return;
 
-        const desc = mc.getElementsByClassName('description')[0];
-
-        mc.style.maxWidth = '99%';
-        mc.style.maxHeight = `${0.85 * window.innerHeight + desc.getBoundingClientRect().height}px`;
-
-        const a = desc.getElementsByTagName('a')[0];
-        a.style.width = 'auto';
-
         const expand = mc.getElementsByClassName('expand')[0];
         if (expand)
             expand.remove();
         mc.classList.remove('too-long-pic');
 
-        const gif = mc.querySelectorAll('p.icon-media.gif')[0];
+        const gif = mc.querySelector('p.icon-media.gif');
         if (gif)
             gif.remove();
 
-        let src = a.href;
-        if (bRemoveWatermark && typeof src !== 'undefined')
-            src = src.replace(/^(https?:\/\/\w\d+\.cdn\d+\.imgwykop\.pl\/.+),.+(\.\w{1,4})(.?)+$/, '$1$2');
+        const a = mc.querySelector(':scope > .description > a');
+        a.style.width = 'auto';
 
         const img = mc.getElementsByTagName('img')[0];
         img.style.maxWidth = '99%';
-        img.style.maxHeight = `${0.85 * window.innerHeight}px`;
+        img.style.maxHeight = '85vh';
         img.classList.remove('lazy');
-        img.src = src;
+
+        const setCleanSrc = (str) => {
+            const m = /^(https?:\/\/(?:www\.)?wykop\.pl\/[^,]+)(?:,\w+)*(\.\w+)$/.exec(str);
+            if (!m)
+                return false;
+            img.src = m.slice(1).join('');
+            return true;
+        };
+
+        if (!setCleanSrc(a.href)) {
+            if (bUseExternalSourceUrl) {
+                const load = new Image();
+                load.onload = () => img.parentNode.href = img.src = load.src;
+                load.onerror = () => setCleanSrc(img.src);
+                load.src = a.href;
+            } else {
+                setCleanSrc(img.src);
+            }
+        }
     };
 
     const isInteresting = (a) => a.rel !== 'lightbox[w]'
             && (bLoadImg && /\.(jpg|jpeg|png|gif)/i.test(a.href)
                 || bLoadVid && /(youtube\.|youtu\.be|vimeo\.com)/i.test(a.href));
     const clickMCEntry = (a) => isInteresting(a) && a.click();
-    const clickMCEntries = (root) => [...root.querySelectorAll('.iC .media-content > a:first-child')].forEach(clickMCEntry);
+    const clickMCEntries = (root) => [...root.querySelectorAll(':scope .iC .media-content > a:first-child')].forEach(clickMCEntry);
 
     const hookMCEntry = (mc) => {
         // Separate observer for each element so we can disconnect them later.
@@ -76,9 +81,9 @@
         if (auto)
             clickMCEntry(mc.getElementsByTagName('a')[0]);
     };
-    const hookMCEntries = (root) => [...root.querySelectorAll('.iC .media-content')].forEach(hookMCEntry);
+    const hookMCEntries = (root) => [...root.getElementsByClassName('media-content')].forEach(hookMCEntry);
 
-    const nav = document.querySelectorAll('.nav.bspace.rbl-block')[0] || document.querySelectorAll('.nav.fix-b-border')[0];
+    const nav = document.querySelector('.nav.bspace.rbl-block') || document.querySelector('.nav.fix-b-border');
     if (nav)
         nav.insertAdjacentHTML('afterbegin', '<ul><li><a id="btnAutoLoad" href="javascript: void(0)">auto</a></li><li><a id="btnLoadImg" href="javascript: void(0)">pokaż obrazki</a></li></ul>');
 
@@ -92,11 +97,7 @@
 
     document.getElementById('btnLoadImg').onclick = (e) => clickMCEntries(e.target.closest('div.grid-main'));
 
-    for (const mc of document.querySelectorAll('.iC .media-content')) {
-        // Separate observer for each element so we can disconnect them later.
-        hookMCEntry(mc);
-    }
-
+    hookMCEntries(document);
     const itemsStream = document.getElementById('itemsStream');
     if (itemsStream) {
         const observer = new MutationObserver((mutationsList) => {
